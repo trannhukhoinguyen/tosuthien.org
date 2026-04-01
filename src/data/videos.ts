7ef17e8ab23e3439a707e2d9be526791d1af93a0
trann <trannhukhoinguyen@gmail.com>
2026-04-01 14:39:39 +0700

    update YouTubePlaylist.astro

---
+++ src/data/videos.ts
@@ -0,0 +1,48 @@
+export interface video {
+    id: string;
+    title: string;
+    url: string;
+    thumbnail: string;
+}
+
+
+const videos: video[] = [
+    {
+        id: 'Me3Sr2lLTiU',
+        title: 'Phật pháp và khoa học - Phần 1 - HT. Thích Duy Lực',
+        url: 'https://www.youtube.com/watch?v=Me3Sr2lLTiU&list=PLA7qBSMZgp0xALxzJB0K4Eg9-RBLtMr68&index=2',
+        thumbnail: 'https://i.ytimg.com/vi/Me3Sr2lLTiU/hqdefault.jpg?sqp=-oaymwEnCPYBEIoBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLA4xhqgu8gnHLGTD6oxmgiiVqnR1A'
+    },
+    {
+        id: 'u6Txq7yBXKk',
+        title: 'Phật pháp và khoa học - Phần 2 - HT. Thích Duy Lực',
+        url: 'https://www.youtube.com/watch?v=u6Txq7yBXKk&list=PLA7qBSMZgp0xALxzJB0K4Eg9-RBLtMr68&index=2',
+        thumbnail: 'https://i.ytimg.com/vi/u6Txq7yBXKk/hqdefault.jpg?sqp=-oaymwEnCPYBEIoBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLA4xhqgu8gnHLGTD6oxmgiiVqnR1A'
+    },
+    {
+        id: 'q4WcvFe50So',
+        title: 'Phương Pháp Tự Ngộ của Thiền Tông - Phần 1 - HT. Thích Duy Lực',
+        url: 'https://www.youtube.com/watch?v=q4WcvFe50So&list=PLA7qBSMZgp0xALxzJB0K4Eg9-RBLtMr68&index=2',
+        thumbnail: 'https://i.ytimg.com/vi/q4WcvFe50So/hqdefault.jpg?sqp=-oaymwEnCPYBEIoBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLA4xhqgu8gnHLGTD6oxmgiiVqnR1A'
+    },
+    {
+        id: '2VBeIWVb6L4',
+        title: 'Phương Pháp Tự Ngộ của Thiền Tông - Phần 2 - HT. Thích Duy Lực',
+        url: 'https://www.youtube.com/watch?v=2VBeIWVb6L4&list=PLA7qBSMZgp0xALxzJB0K4Eg9-RBLtMr68&index=2',
+        thumbnail: 'https://i.ytimg.com/vi/2VBeIWVb6L4/hqdefault.jpg?sqp=-oaymwEnCPYBEIoBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLA4xhqgu8gnHLGTD6oxmgiiVqnR1A'
+    },
+    {
+        id: 'Yj4N56wqZgA',
+        title: 'Phương Pháp Tự Ngộ của Thiền Tông - Phần 3 - HT. Thích Duy Lực',
+        url: 'https://www.youtube.com/watch?v=Yj4N56wqZgA&list=PLA7qBSMZgp0xALxzJB0K4Eg9-RBLtMr68&index=2',
+        thumbnail: 'https://i.ytimg.com/vi/Yj4N56wqZgA/hqdefault.jpg?sqp=-oaymwEnCPYBEIoBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLA4xhqgu8gnHLGTD6oxmgiiVqnR1A'
+    },
+    {
+        id: 'ZPPoYEbEOpg',
+        title: 'Phương Pháp Tự Ngộ của Thiền Tông - Phần 4 - HT. Thích Duy Lực',
+        url: 'https://www.youtube.com/watch?v=ZPPoYEbEOpg&list=PLA7qBSMZgp0xALxzJB0K4Eg9-RBLtMr68&index=2',
+        thumbnail: 'https://i.ytimg.com/vi/ZPPoYEbEOpg/hqdefault.jpg?sqp=-oaymwEnCPYBEIoBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLA4xhqgu8gnHLGTD6oxmgiiVqnR1A'
+    },
+];
+
+export default videos;
