b60434cf6cffc17cc574cc8098299069ffacb78f
trann <trannhukhoinguyen@gmail.com>
2026-05-06 13:36:45 +0700

    update

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -7,7 +7,7 @@ export interface video {
 
 
 const videos: video[] = [
-    {
+/*    {
         id: 'Me3Sr2lLTiU',
         title: 'Phật pháp và khoa học - Phần 1 - HT. Thích Duy Lực',
         url: 'https://www.youtube.com/watch?v=Me3Sr2lLTiU&list=PLA7qBSMZgp0xALxzJB0K4Eg9-RBLtMr68&index=2',
@@ -42,6 +42,74 @@ const videos: video[] = [
         title: 'Phương Pháp Tự Ngộ của Thiền Tông - Phần 4 - HT. Thích Duy Lực',
         url: 'https://www.youtube.com/watch?v=ZPPoYEbEOpg&list=PLA7qBSMZgp0xALxzJB0K4Eg9-RBLtMr68&index=2',
         thumbnail: 'https://i.ytimg.com/vi/ZPPoYEbEOpg/hqdefault.jpg?sqp=-oaymwEnCPYBEIoBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLA4xhqgu8gnHLGTD6oxmgiiVqnR1A'
+    },*/
+    {
+        id: 'jifZqkkcx-Y',
+        title: 'PHƯƠNG PHÁP TỰ NGỘ CỦA THIỀN TÔNG (1/5)',
+        url: 'https://www.youtube.com/watch?v=jifZqkkcx-Y',
+        thumbnail: 'https://i.ytimg.com/vi/jifZqkkcx-Y/hqdefault.jpg?sqp=-oaymwEmCKgBEF5IWvKriqkDGQgBFQAAiEIYAdgBAeIBCggYEAIYBjgBQAE=&rs=AOn4CLANUEuWIjjjn9qMh4_yzM9y6sLs9A'
+    },
+    {
+        id: 'lEFaHfat-4I',
+        title: 'PHƯƠNG PHÁP TỰ NGỘ CỦA THIỀN TÔNG (2/5)',
+        url: 'https://www.youtube.com/watch?v=lEFaHfat-4I',
+        thumbnail: 'https://i.ytimg.com/vi/XOO5rtXN6sk/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLD8eYls6lHQYRxQWWBdg9GcSidoeg'
+    },
+    {
+        id: 'XOO5rtXN6sk',
+        title: 'PHƯƠNG PHÁP TỰ NGỘ CỦA THIỀN TÔNG (3/5)',
+        url: 'https://www.youtube.com/watch?v=XOO5rtXN6sk',
+        thumbnail: 'https://i.ytimg.com/vi/XOO5rtXN6sk/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLD8eYls6lHQYRxQWWBdg9GcSidoeg'
+    },
+    {
+        id: 'X0jHLgyr5vE',
+        title: 'PHƯƠNG PHÁP TỰ NGỘ CỦA THIỀN TÔNG (4/5)',
+        url: 'https://www.youtube.com/watch?v=X0jHLgyr5vE',
+        thumbnail: 'https://i.ytimg.com/an_webp/X0jHLgyr5vE/mqdefault_6s.webp?du=3000&sqp=CIzm6s8G&rs=AOn4CLBGd1jWXYRx2kK2dSU-2qjHEVjFJQ'
+    },
+    {
+        id: 'ZyQiSC4D3Fc',
+        title: 'PHƯƠNG PHÁP TỰ NGỘ CỦA THIỀN TÔNG (5/5)',
+        url: 'https://www.youtube.com/watch?v=ZyQiSC4D3Fc',
+        thumbnail: 'https://i.ytimg.com/vi/ZyQiSC4D3Fc/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLAeJtfGwoajeayV7XpV95g-NWHPsw'
+    },
+
+    {
+        id: 'SImIIV9w_fc',
+        title: 'THUYẾT GIẢNG TẠI WESTMINTER (PHẦN 1/2)',
+        url: 'https://www.youtube.com/watch?v=SImIIV9w_fc',
+        thumbnail: 'https://i.ytimg.com/an_webp/HyOSIgEZxhY/mqdefault_6s.webp?du=3000&sqp=CODg6s8G&rs=AOn4CLA1geevoK4OaLKRDSC0Mqaz50Ab0w'
+    },
+    {
+        id: 'DpACZzhsUv4',
+        title: 'THUYẾT GIẢNG TẠI WESTMINTER (PHẦN 2/2)',
+        url: 'https://www.youtube.com/watch?v=DpACZzhsUv4',
+        thumbnail: 'https://i.ytimg.com/an_webp/DpACZzhsUv4/mqdefault_6s.webp?du=3000&sqp=CO7F6s8G&rs=AOn4CLAdtHvGTEASq0CDRq116AbupJ8VJA'
+    },
+
+    {
+        id: 'gEgCS0fGHUw',
+        title: '132. Khai thị cách Thực Hành Tham Thoại Đầu',
+        url: 'https://www.youtube.com/watch?v=gEgCS0fGHUw',
+        thumbnail: 'https://i.ytimg.com/vi/gEgCS0fGHUw/hqdefault.jpg?sqp=-oaymwFBCNACELwBSFryq4qpAzMIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB8AEB-AHUBoACrAOKAgwIABABGH8gRigTMA8=&rs=AOn4CLDluxc2RF5Qy429MrfUrPw_cepIAA'
+    },
+    {
+        id: 'hgNUMEIT2Lw',
+        title: '2. Cơ Bản Thực Hành tham Tổ Sư Thiền',
+        url: 'https://www.youtube.com/watch?v=hgNUMEIT2Lw',
+        thumbnail: 'https://i.ytimg.com/an_webp/hgNUMEIT2Lw/mqdefault_6s.webp?du=3000&sqp=CPT66s8G&rs=AOn4CLAgke9MLqXb4rgklIXOpAz1_NH_ww'
+    },
+    {
+        id: 'eE4aZSU8Vck',
+        title: '290. Khai Thị Đường Lối Thực Hành Tham Tổ Sư Thiền',
+        url: 'https://www.youtube.com/watch?v=eE4aZSU8Vck',
+        thumbnail: 'https://i.ytimg.com/vi/eE4aZSU8Vck/hqdefault.jpg?sqp=-oaymwFBCNACELwBSFryq4qpAzMIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB8AEB-AHUBoACrAOKAgwIABABGH8gRygTMA8=&rs=AOn4CLDqWobRmiOzflV5eEDcAzklyA32Zg'
+    },
+    {
+        id: '2SOr9XrbPi4',
+        title: '563. Xin Sư Phụ khai thị đường lối thực hành tham Tổ Sư Thiền cho con thực hành',
+        url: 'https://www.youtube.com/watch?v=2SOr9XrbPi4',
+        thumbnail: 'https://i.ytimg.com/an_webp/2SOr9XrbPi4/mqdefault_6s.webp?du=3000&sqp=CMqK688G&rs=AOn4CLD7y1rEFw-ylqRwdR4ypKVRV14Y-w'
     },
 ];
 
